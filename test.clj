f46832f7813b4c54cda0932bc25b94f5a37322e4
Mike Plavsky <Mike.Plavsky@quest.com>
2012-05-23 17:17:36 +0000

    .gitignore

--- test.clj
+++ test.clj
@@ -3,4 +3,11 @@
 (defn average [numbers]
   (/ (apply + numbers) (count numbers))) 
 
-(pr (average (read-string "[10 2 3 4 7]")))
+(println (average (read-string "[10 2 3 4 7]")))
+
+(def person {:name "Ken Simpson" :city "SPB"})
+(println (:city person))
+
+(println (re-seq #"^(.*)\s+(.*)$" (:name person)))
+
+(println (= [1 2 3] [1, 2, 3]))
